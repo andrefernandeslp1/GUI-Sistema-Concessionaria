9738646f6b60fdb2faf4dc66c9ccaee8fae79787
andrefernandeslp1 <andre.mdia@gmail.com>
2023-07-17 11:45:10 -0300

    Tamanho

--- src/AppGUI.java
+++ src/AppGUI.java
@@ -36,7 +36,7 @@ public class AppGUI extends JFrame {
         //createNovaLojaPanel();
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
+        setSize(600, 400);
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
